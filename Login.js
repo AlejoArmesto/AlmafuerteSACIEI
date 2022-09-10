7898f7fb4f78c6800d0f56174851c74312b88e88
AlejoArmesto <aleezequiel94@gmail.com>
2022-09-10 15:10:53 -0300

    agregando js al proyecto

---
+++ Login.js
@@ -0,0 +1,8 @@
+let nombre;
+let apellido;
+let resultado;
+
+nombre = prompt ("Ingrese su nombre");
+apellido = prompt ("Ingrese su apellido");
+resultado = nombre + apellido;
+alert("Bienvenido! " + resultado);
