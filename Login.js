ac2cd96eafe426868b2997319a2023c57a43f571
AlejoArmesto <aleezequiel94@gmail.com>
2022-09-10 15:16:28 -0300

    corrigiendo el js

--- Login.js
+++ Login.js
@@ -4,5 +4,5 @@ let resultado;
 
 nombre = prompt ("Ingrese su nombre");
 apellido = prompt ("Ingrese su apellido");
-resultado = nombre + apellido;
-alert("Bienvenido! " + resultado);
+resultado = nombre + " " + apellido;
+alert("Bienvenido/a! " + resultado);
